b66a199d285aaa0092ddcfa13d4d276247af07fc
Bruno Modinuti <modinutidev@gmail.com>
2022-01-28 08:39:15 -0300

    :tada: Adicionando códigos

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from "react";
+import {View, Text, FlatList, Image, StyleSheet} from "react-native";
+import api from "./src/services/api";
+
+export default function App() {
+  const [movies, setMovies] = useState(null);
+
+  async function getMovies() {
+    try {
+      const response = await api.get(`r-api/?api=filmes`);
+      setMovies(response.data);
+    } catch (error) {
+      alert(`Não foi possível conectar-se ao servidor! ${error}`);
+    }
+  }
+  
+  useEffect(
+    () => {
+      getMovies();
+    }
+  );
+    
+  return(
+    <View style={styles.container}>
+      <FlatList 
+        data={movies}
+        keyExtractor={(item)=>item.id.toString()}
+        renderItem={({item}) => ComponentMovie(item)}
+      />
+    </View>
+  )
+}
+
+function ComponentMovie(movie) {
+  return(
+    <View style={styles.movie}>
+      <Text style={styles.movie__title}>{movie.nome}</Text>
+      <Image 
+        style={styles.movie__image}
+        source={{uri: movie.foto}}
+      />
+      <Text style={styles.movie__sinopse}>    {movie.sinopse}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#322924'},
+  movie: {backgroundColor: '#251e1a', marginTop: 5, marginHorizontal: 10, marginBottom: 5, padding: 10, borderRadius: 10},
+  movie__title: {fontSize: 18, fontWeight: '800', color: '#aaa', textTransform: 'uppercase', marginBottom: 10},
+  movie__image: { width: '100%', height: 300, borderRadius: 10},
+  movie__sinopse: {fontSize: 12, color: '#aaa', textAlign: 'justify', marginTop: 10}
+});
