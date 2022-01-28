ba46a469c12bfc4a66a2951d5e412bb11bc1546e
Bruno Modinuti <modinutidev@gmail.com>
2022-01-28 19:17:00 -0300

    :sunglasses: Adicionado ActivityIndicator

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {View, Text, FlatList, Image, StyleSheet} from "react-native";
+import {View, Text, FlatList, Image, StyleSheet, ActivityIndicator} from "react-native";
 import api from "./src/services/api";
 
 export default function App() {
@@ -19,14 +19,23 @@ export default function App() {
       getMovies();
     }
   );
-    
+
   return(
     <View style={styles.container}>
-      <FlatList 
-        data={movies}
-        keyExtractor={(item)=>item.id.toString()}
-        renderItem={({item}) => ComponentMovie(item)}
-      />
+      { movies ?
+        <FlatList 
+          data={movies}
+          keyExtractor={(item)=>item.id.toString()}
+          renderItem={({item}) => ComponentMovie(item)}
+        />
+      :
+        <View style={styles.loading}>
+          <ActivityIndicator 
+            size={40}
+            color={'#aaa'}
+          />
+        </View>
+      }
     </View>
   )
 }
@@ -46,6 +55,8 @@ function ComponentMovie(movie) {
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: '#322924'},
+  loading: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  loading__indicator: {},
   movie: {backgroundColor: '#251e1a', marginTop: 5, marginHorizontal: 10, marginBottom: 5, padding: 10, borderRadius: 10},
   movie__title: {fontSize: 18, fontWeight: '800', color: '#aaa', textTransform: 'uppercase', marginBottom: 10},
   movie__image: { width: '100%', height: 300, borderRadius: 10},
